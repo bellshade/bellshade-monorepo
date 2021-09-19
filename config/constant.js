8d04c5c8ae61fe22009039b59115e289b8600c84
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-09-19 13:56:36 +0700

    Menambahkan file constant dan fitur cache data ke memory

---
+++ config/constant.js
@@ -0,0 +1,5 @@
+const GITHUB_CACHE_KEY = "gh_bellshade_cache";
+
+module.exports = {
+  GITHUB_CACHE_KEY,
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const NodeCache = require("node-cache");
 const cors = require("cors");
 
 const { getMembers, getUser } = require("./utils/github.api");
+const { GITHUB_CACHE_KEY } = require("./config/constant");
 
 const app = express();
 const bellshadeCache = new NodeCache();
@@ -12,6 +13,10 @@ app.use(compression());
 app.use(cors());
 
 app.get("/", (req, res) => {
+  const dataCache = bellshadeCache.get(GITHUB_CACHE_KEY);
+
+  if (dataCache) return res.json(dataCache);
+
   getMembers()
     .then((data) => {
       const refetch = data.map((users) => getUser(users.login));
@@ -26,13 +31,12 @@ app.get("/", (req, res) => {
         name: member.name,
       }))
     )
-    .then((data) => res.json(data))
-    .catch((error) => {
-      res.status(503).json({
-        error,
-        message: "Error while fetching member data github api",
-      });
-    });
+    .then((data) => {
+      bellshadeCache.set(GITHUB_CACHE_KEY, data, 3600 * 5);
+      res.json(data);
+    })
+    .catch((error) => res.status(error.status).json(error.response.data));
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
