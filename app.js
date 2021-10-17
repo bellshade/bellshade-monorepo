c0f586f6c5964540f510a72c0b23dddd61559d97
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-17 12:37:00 +0700

    Memindahkan listening ke file server dan menambahkan cache pre handler

---
+++ app.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 3000;
+const { leaderboard, main } = require("./routes");
+
+const fastify = require("fastify")({
+  logger: process.env.NODE_ENV !== "production",
+});
+
+// plugin
+fastify.register(require("fastify-compression"));
+fastify.register(require("fastify-cors"), require("./config/cors"));
+fastify.register(require("./plugin/cacheAndConstant"));
+
+const cachePreHandler = require("./common/cachePreHandler")(fastify);
+
+// routing
+fastify.register(main(cachePreHandler), { prefix: "/" });
+fastify.register(leaderboard(cachePreHandler), { prefix: "/leaderboard" });
+
+module.exports = fastify;
--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-const PORT = process.env.PORT || 3000;
-const { leaderboard, main } = require("./routes");
-
-const fastify = require("fastify")({
-  logger: process.env.NODE_ENV !== "production",
-});
-
-// plugin
-fastify.register(require("fastify-compression"));
-fastify.register(require("fastify-cors"), require("./config/cors"));
-fastify.register(require("./plugin/cacheAndConstant"));
-
-// routing
-fastify.register(main, { prefix: "/" });
-fastify.register(leaderboard, { prefix: "/leaderboard" });
+const app = require("./app");
 
 const start = async () => {
   try {
-    await fastify.listen(PORT, "0.0.0.0");
+    await app.listen(PORT, "0.0.0.0");
     console.log(`Listening on port ${PORT}`);
   } catch (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
+
 start();
