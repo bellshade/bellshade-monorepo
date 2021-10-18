db6ac8ad91fb2a9eb08afbdd7c9a25677f165ee3
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-18 07:36:22 +0700

    Konsistensi kode untuk promise diusahakan menghindari async await

--- github/getter/getAllMembersInfo.js
+++ github/getter/getAllMembersInfo.js
@@ -1,21 +1,15 @@
 const { getMembers, getUser } = require("../fetcher");
 
 const getAllMembersInfo = () =>
-  getMembers().then(async (data) => {
-    try {
-      const members = await Promise.all(
-        data.map((users) => getUser(users.login))
-      );
-
-      return members.map((member) => ({
+  getMembers().then((data) =>
+    Promise.all(data.map((users) => getUser(users.login))).then((members) =>
+      members.map((member) => ({
         login: member.login,
         name: member.name,
         html_url: member.html_url,
         avatar_url: member.avatar_url,
-      }));
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  });
+      }))
+    )
+  );
 
 module.exports = getAllMembersInfo;
--- github/getter/getOrgContributors.js
+++ github/getter/getOrgContributors.js
@@ -11,40 +11,40 @@ const getOrgContributors = () =>
 
       return Promise.all(
         // get all contributors
-        repos.map(async (repo) => {
-          const allContributors = await getRepoContributors(repo);
+        repos.map((repo) =>
+          getRepoContributors(repo).then((allContributors) => {
+            // Filter the bot
+            const contrib = {
+              repo,
+              contributors: allContributors
+                .filter(({ type }) => type !== "Bot") // Eliminate bot
+                .map((data) => ({
+                  login: data.login,
+                  contributions: data.contributions,
+                })),
+            };
 
-          // Get all contributor, and filter the bot
-          const contrib = {
-            repo,
-            contributors: allContributors
-              .filter(({ type }) => type !== "Bot") // Eliminate bot
-              .map((data) => ({
-                login: data.login,
-                contributions: data.contributions,
-              })),
-          };
-
-          // Refetch user info
-          const reftechUser = await Promise.all(
-            contrib.contributors.map(({ login, contributions }) =>
-              getUser(login).then(({ login, avatar_url, html_url, name }) => ({
-                user: {
-                  login,
-                  name,
-                  html_url,
-                  avatar_url,
-                },
-                contributions,
-              }))
-            )
-          );
-
-          return {
-            repo: contrib.repo,
-            contributors: reftechUser,
-          };
-        })
+            // Refetch user info
+            return Promise.all(
+              contrib.contributors.map(({ login, contributions }) =>
+                getUser(login).then(
+                  ({ login, avatar_url, html_url, name }) => ({
+                    user: {
+                      login,
+                      name,
+                      html_url,
+                      avatar_url,
+                    },
+                    contributions,
+                  })
+                )
+              )
+            ).then((reftechUser) => ({
+              repo: contrib.repo,
+              contributors: reftechUser,
+            }));
+          })
+        )
       );
     });
 
