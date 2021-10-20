e4a604f908df9d91b1243dfdbd032f60619a6a3b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-20 14:35:37 +0700

    mengembalikan auth

--- helpers/octokit.js
+++ helpers/octokit.js
@@ -5,7 +5,7 @@ const { Octokit } = require("@octokit/rest");
 const githubApiWrapper = (auth) => {
   if (!auth) throw new Error("GITHUB TOKEN REQUIRED!");
 
-  return new Octokit();
+  return new Octokit({ auth });
 };
 
 module.exports = githubApiWrapper(process.env.GITHUB_TOKEN_API);
